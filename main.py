30960186dcfc1a18c8503a5dc80745d9d1b8f7e8
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-02-26 02:17:02 -0800

    refactor: change print format and move to separate function

--- main.py
+++ main.py
@@ -7,22 +7,26 @@ def main():
     word_count = get_word_count(text)
     chars_dict = get_chars_dict(text)
     chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{word_count} words found in the document")
-    print()
-
-    for item in chars_sorted_list:
-        if not item["char"].isalpha():
-            continue
-        print(f"The '{item['char']}' character was found {item['num']} times")
-
-    print("--- End report ---")
+    print_report(book_path, word_count, chars_sorted_list)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 
 main()
